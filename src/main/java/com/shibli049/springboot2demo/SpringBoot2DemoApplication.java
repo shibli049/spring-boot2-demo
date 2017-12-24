396073d1e4975c61ffa22890430ef02bac841a2f
Tahmim Ahmed Shibli <shibli05@gmail.com>
2017-12-24 17:10:38 +0600

    initial.

---
+++ src/main/java/com/shibli049/springboot2demo/SpringBoot2DemoApplication.java
@@ -0,0 +1,12 @@
+package com.shibli049.springboot2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2DemoApplication.class, args);
+	}
+}
